868e6dfe662c20bff61eacfeb96894a0f443a681
liuchang-python <2585408754@qq.com>
2021-01-06 00:49:27 +0800

    000

--- src/components/Add_user.vue
+++ src/components/Add_user.vue
@@ -11,16 +11,16 @@
                 <div class="grid-content bg-purple">
                     <el-form ref="form" :model="user" label-width="80px">
                         <el-form-item label="姓名">
-                            <el-input v-model="user.username"></el-input>
+                            <el-input v-model="user.username"><i slot="prefix" class="el-icon-user"></i></el-input>
                         </el-form-item>
                         <el-form-item label="密码">
-                            <el-input v-model="user.password"></el-input>
+                            <el-input v-model="user.password"><i slot="prefix" class="el-icon-lock"></i></el-input>
                         </el-form-item>
                         <el-form-item label="年龄">
-                            <el-input v-model="user.age"></el-input>
+                            <el-input v-model="user.age"><i slot="prefix" class="el-icon-sunny"></i></el-input>
                         </el-form-item>
                         <el-form-item label="地址">
-                            <el-input v-model="user.address"></el-input>
+                            <el-input v-model="user.address"><i slot="prefix" class="el-icon-map-location"></i></el-input>
                         </el-form-item>
                     </el-form>
 
